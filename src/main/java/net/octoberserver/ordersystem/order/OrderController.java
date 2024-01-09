cf88342c729b28aa51f2900df6afa7b295555afc
october <octoberserver@gmail.com>
2024-01-09 17:49:35 +0800

    Fixed controller path error

--- src/main/java/net/octoberserver/ordersystem/order/OrderController.java
+++ src/main/java/net/octoberserver/ordersystem/order/OrderController.java
@@ -7,10 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
-import java.util.Objects;
-import java.util.Optional;
-
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/order")
@@ -18,7 +14,7 @@ public class OrderController {
 
     private final OrderService orderService;
 
-    @GetMapping("/")
+    @GetMapping
     public GetOrderDataDAO getOrderData() {
         return orderService.getOrderData(Long.parseLong(SecurityContextHolder.getContext().getAuthentication().getName()));
     }
