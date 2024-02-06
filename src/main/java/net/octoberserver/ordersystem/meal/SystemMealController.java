cc37a212a936d4bf4ba3b02e17642d374ff1eb87
october <octoberserver@gmail.com>
2024-02-06 21:20:16 +0800

    .

--- src/main/java/net/octoberserver/ordersystem/meal/SystemMealController.java
+++ src/main/java/net/octoberserver/ordersystem/meal/SystemMealController.java
@@ -1,6 +1,5 @@
 package net.octoberserver.ordersystem.meal;
 
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
