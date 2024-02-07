dec611c714c92e565561dcdddd2b551f07335f1d
october <octoberserver@gmail.com>
2024-02-07 16:05:21 +0800

    .

--- src/main/java/net/octoberserver/ordersystem/OrderSystemApplication.java
+++ src/main/java/net/octoberserver/ordersystem/OrderSystemApplication.java
@@ -2,12 +2,25 @@ package net.octoberserver.ordersystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class OrderSystemApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(OrderSystemApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/").allowedOrigins("https://zl-order-system.github.io");
+            }
+        };
     }
 }
 
