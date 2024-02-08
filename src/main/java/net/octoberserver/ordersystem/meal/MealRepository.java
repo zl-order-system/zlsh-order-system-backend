04904a946db4397de52d514c4454380d05124e38
october <octoberserver@gmail.com>
2024-02-08 20:46:28 +0800

    .

--- src/main/java/net/octoberserver/ordersystem/meal/MealRepository.java
+++ src/main/java/net/octoberserver/ordersystem/meal/MealRepository.java
@@ -7,6 +7,6 @@ import java.time.LocalDate;
 import java.util.List;
 
 public interface MealRepository extends JpaRepository<Meal, LocalDate>  {
-    @Query("SELECT m.date FROM Meal m WHERE m.date > :today ORDER BY m.date ASC LIMIT 5")
+    @Query("SELECT m.date FROM Meal m WHERE m.date >= :today ORDER BY m.date ASC LIMIT 5")
     List<LocalDate> findUpcomingDates(LocalDate today);
 }
