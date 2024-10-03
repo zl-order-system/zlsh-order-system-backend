f8330d24e22796ff2f3f7c28bc75bd15c8dc82ad
october <octoberserver@gmail.com>
2024-10-03 13:32:00 +0800

    .

--- src/main/java/net/octoberserver/ordersystem/websocket/WebSocketConfig.java
+++ src/main/java/net/octoberserver/ordersystem/websocket/WebSocketConfig.java
@@ -15,7 +15,7 @@ public class WebSocketConfig implements WebSocketConfigurer {
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(applicationWebSocketHandler, "/api/order/realtime")
+        registry.addHandler(applicationWebSocketHandler, "/ws")
             .setAllowedOrigins("*");
     }
 
